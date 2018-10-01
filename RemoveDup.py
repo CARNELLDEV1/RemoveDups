fc8afe36504b3593506a8188ae044406b36b59ca
[name] <hao.ye@carnellgroup.co.uk>
2018-10-01 15:33:55 +0100

    01/10/2018

--- RemoveDup.py
+++ RemoveDup.py
@@ -1,23 +1,52 @@
+###############################################################
+#
+#
+#
+###############################################################
+
 import arcpy
 import os
 
-# Configuration
+###############################################################
+# Configuration for database server and local filedatabase
+###############################################################
 
 CONFIG = {
     'SDE':r'Database Connections\Connection to carnell99 (3).sde',
     'newJobName':'Points_Version',
-    'LocalPath':r'C:\Users\haoye\OneDrive - Carnell Support Services Ltd\Desktop\Projects\RemoveDup\Data'
+    'LocalPath':r'C:\Users\haoye\OneDrive - Carnell Support Services Ltd\Desktop\Projects\RemoveDup\Data\Data.shp'
     }
 
+################################################################
 # Configure the workspace for SDE
+################################################################
 arcpy.env.workspace = CONFIG['SDE']
 jobName = CONFIG['newJobName']
-outfile = CCONFIG['LocalPath']
+outfile = CONFIG['LocalPath']
 
-# Configure the names
+################################################################
+# Configure the name matching comparison
+################################################################
+
+mxd = arcpy.mapping.MapDocument("CURRENT")
+df = arcpy.mapping.ListDataFrames(mxd)[0]
+for lyr in arcpy.mapping.ListLayers(mxd, "", df):
+    if lyr.name == "Data":
+        arcpy.mapping.MoveLayer(df,lyr)
+mxd.saveACopy(r"C:\Project\Project2.mxd")
+del mxd
+
 ly_names = arcpy.ListFeatureClasses()
 matching = [fc for fc in ly_names if jobName in fc]
 
+if os.path.isfile(outfile):
+    os.remove(outfile)
+
 for ly in matching:
-    infile = arcpy.env.workspace +"\\" + ly
+    infile = os.path.join(arcpy.env.workspace,ly)
     arcpy.CopyFeatures_management(infile,outfile)
+
+#################################################################
+#
+#################################################################
+
